27f2bd72d18e25e618157336884ffd029a06dfb0
Brandon Swanson <github@brandonswanson.com>
2014-07-16 15:30:49 -0700

    dictionary

---
+++ Dictionary.hpp
@@ -0,0 +1,228 @@
+/***********************************************************
+ * Author: Brandon Swanson
+ * Date Created: 07-13-2014
+ * Last Modification Date: 07-13-2014
+ * Filename: Dictionary.hpp
+ *
+ * Overview: Create a searchable map of words
+ *
+ * Input: a file with alpha only, lower case, return character separated words
+ *
+ * Output: a dictionary object that can be polled for number of words contained
+ * and if a single string element exists in the dictionary
+ *
+ ***********************************************************/
+
+#ifndef DICTIONARY_HPP_
+#define DICTIONARY_HPP_
+
+#include <fstream>
+#include <string>
+#include <set>
+#include <algorithm> //for string compare less<string>, used by set<>
+#include <iterator>
+#include "swansonUtils.hpp"
+#include "swansonString.hpp"
+
+
+using namespace std;
+
+class Dictionary {
+
+private:
+   set<string> wordSet;
+   set<string>::iterator lookup;
+
+protected:
+   /**************************************************************
+    *
+    * * Entry: a file with only alpha, lower case, return character separated
+    *          words
+    *
+    * * Exit: wordSet is filled with unique and sorted strings
+    *          alpha only, lowercase, excluding ones greater than max length
+    *
+    * * Purpose: to parse a file into WORDS, for dictionary data set
+    *
+    *
+    * ***************************************************************/
+   virtual void ReadFromFile ( fstream &instream , set<string> &wordSet ,
+         const int MaxWordLength ) {
+
+      string nextWord;
+
+      while ( !instream.eof() ) {
+         getline( instream , nextWord ); // Retrieve next word
+         if ( MaxWordLength == UNRESTRICTED
+               || nextWord.size() <= MaxWordLength ) {
+            wordSet.insert( nextWord );
+         }
+      }
+
+   }
+
+   /**************************************************************
+    *
+    * * Entry: an existing file at <filename>
+    *
+    * * Exit: an inflated map
+    *
+    * * Purpose:  open and close dictionary file, report failures to inflate
+    *    data set
+    *
+    * ***************************************************************/
+   bool InflateDict ( string filename , const int MaxWordLength ) {
+
+      fstream instream;
+
+      //open dictionary file
+      instream.open( filename.c_str() );
+
+      if ( instream.fail() ) {
+         return false;
+      }
+
+      //read words from file
+      this->ReadFromFile( instream , wordSet , MaxWordLength );
+
+      //words added  //close file
+      instream.close();
+
+      if ( wordSet.empty() )
+         return false;
+
+      return true;
+
+   }
+
+public:
+   bool constructionWasSuccesfull;
+   static const int UNRESTRICTED = -1;
+
+   ///constructors
+   Dictionary ( bool dummy ) {
+      constructionWasSuccesfull = false; //used to instantiate an empty dictionary
+   }
+   Dictionary ( int maxWordLenght = UNRESTRICTED , string filename =
+         "dictionary.txt" , bool construct = true) {
+      //inflates on construction by default
+      //sub classes can override ReadFromFile() method, and must call inflate
+      //in their own constructors
+      if(construct)
+         constructionWasSuccesfull = InflateDict( filename , maxWordLenght );
+      else
+         constructionWasSuccesfull = false;
+   }
+
+   /**************************************************************
+    *
+    * * Entry:an inflated map
+    *
+    *
+    * * Exit:true if map contains 1 or more of element, false otherwise
+    *
+    *
+    * * Purpose: determine if passed in word is a word in dictionary
+    *
+    *
+    * ***************************************************************/
+   bool IsAWord ( string word ) {
+      if ( wordSet.count( word ) > 0 )
+         return true;
+      else
+         return false;
+   }
+
+   //returns number of words
+   long int NumWords () {
+      return wordSet.size();
+   }
+
+   //returns string at a given position
+   //only useful for a specialized generation of random numbers for access
+   string GetWordAt ( int position ) {
+      lookup = wordSet.begin();
+      advance( lookup , position );
+      return *lookup;
+
+   }
+
+   string GetRandomWord () {
+      int position = swansonUtil::GetRandomInRange( NumWords() );
+      return GetWordAt( position );
+   }
+
+};
+
+/**************************************************************
+ * * Purpose: A dictionary object with a non optimized input stream file
+ *            Considerably longer input time but possibly more fun
+ * ***************************************************************/
+
+class ThemeDictionary: public Dictionary {
+public:
+
+   ///constructors
+   ThemeDictionary ( bool dummy ) : Dictionary(dummy){}  //call parent constructor
+   ThemeDictionary ( int maxWordLenght = UNRESTRICTED , string filename =
+         "dictionary.txt" , bool construct = false)
+         :Dictionary(maxWordLenght,filename,false){
+      /*must use this two staged inflation,  base class will by default
+       * inflate upon construction, but then the overridden subclass method
+       * is not called during the inflate method, because the 'this' pointer
+       * is still referencing the base class,  in this constructor the construct
+       * =false flag is sent to the super constructor to prevent it from
+       * inflating using parent method default arguments, then the derived
+       * class inflates with its specialized word parsing method
+      */
+      constructionWasSuccesfull = InflateDict( filename , maxWordLenght );
+   }
+
+
+
+private:
+   /**************************************************************
+    *
+    * * Entry: a file with something in it, preferably at some point in it
+    *          combinations of alpha characters separated by spaces on one or
+    *          both sides, and comprised of intelligible words.
+    *
+    * * Exit: wordSet is filled with unique and sorted strings
+    *          alpha only, lowercase, excluding ones greater than max length
+    *
+    * * Purpose: to parse a file into WORDS, for dictionary data set
+    *
+    *
+    * ***************************************************************/
+   void ReadFromFile ( fstream &instream , set<string> &wordSet ,
+         const int MaxWordLength ) {
+
+      string nextWord;
+
+      while ( !instream.eof() ) {
+         while ( !swansonString::IsALetter( (instream.peek()) ) ) {
+            instream.ignore( 1 );
+         }
+         string nextWord;
+         getline( instream , nextWord , ' ' );
+         //check last character for alpha // for !.?"
+         while ( ! swansonString::IsALetter( nextWord.at(nextWord.size()-1) ) ) {
+            nextWord.erase(nextWord.size()-1,1); // delete last character
+         }
+
+         if ( !nextWord.empty() && swansonString::AllLetters( nextWord ) ) {
+
+            nextWord = swansonString::LowerCase(nextWord);
+            wordSet.insert(nextWord);
+
+         }
+
+      }
+
+   }
+
+
+};
+
+#endif /* DICTIONARY_HPP_ */
+
